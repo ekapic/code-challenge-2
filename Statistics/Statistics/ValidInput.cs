eb0378d0ab0f7943d3581b3b4abb916f99cf431e
Gerard Colomer <gcolomer22@gmail.com>
2015-12-20 20:07:22 +0100

    Program GerardColomer Statistics
    Final Program

---
+++ Statistics/Statistics/ValidInput.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistics
+{
+    public static class ValidInput
+    {
+        public static InputTypes Number;
+       
+        public static bool Validate(InputTypes type, string value)
+        {
+            int number;
+            bool result = false;
+
+            switch (type)
+            {
+                case InputTypes.Number:
+                    result = int.TryParse(value, out number) && number > 0 ? true : false;
+                    break;
+            }
+
+            return result;
+        }
+    }
+        public enum InputTypes
+        {
+            Number
+        }
+    
+}
