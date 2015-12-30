a70c112494c8bb0f60cbe80f7537f8eea37344b7
eddThunder <animalthorne@gmail.com>
2015-12-30 16:27:17 +0100

    addfile and constants

---
+++ CalculoEstadisiticas/CalculoEstadisiticas/Constants.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace CalculoEstadisiticas
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public static class Constants
+    {
+        public const int exitNumber = 0;
+        public const string exitExpression = "done";
+        public const string decimalFormat = "0.00";
+        public const string textfileRoute = "test.txt";
+        public const string textNumberSeparator = "-";
+    }
+}
